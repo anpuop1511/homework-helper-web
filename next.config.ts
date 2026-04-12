892d6ff56b0d5822dea796aa7c73ddc7bc83e577
Anthony Puopolo <anpuop1511@gmail.com>
2026-04-12 19:02:19 -0400

    fix: Expose flutter .env variables safely on Web

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/app/assets/.env',
+        destination: '/app/assets/env.txt',
+      },
+    ];
+  },
   async redirects() {
     return [
       {
